abdf6873f1e4604ef7e1ed1ff3d8a8fb3b08f4fe
Jesse <jesse.holwell@au.ey.com>
2017-10-25 09:35:27 +1100

    initial commit - css based team chart

---
+++ mvcPlayground/mvcPlayground/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcPlayground.MvcApplication" Language="C#" %>
---
+++ mvcPlayground/mvcPlayground/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcPlayground.Startup))]
+namespace mvcPlayground
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
